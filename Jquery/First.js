96429d92d5e7d82c92f613a0af263a0c9496d8c8
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-06-20 07:57:55 +0530

    Create First.html

---
+++ Jquery/First.js
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  </head>
+   <body>
+    <script>
+       $(document).ready(function(){
+          var n1 = $("P")
+
+          for(ele of n1)
+            ele.innerHTML="New Content"
+       })
+    </script>
+     
+     <div id="main">
+      <h1>Welcome to HTML</h1>
+      <p> This is para-1</p>
+       <div>
+            <p>First Paragraph</p>
+            <p>Second Paragraph</p>
+            <p>Third Paragraph</p>
+            <p>Fourth Paragraph</p>
+            <p>Fifth Paragraph</p>
+            <p>Sixth Paragraph</p>
+        </div>
+  	<p>Seventh Paragraph</p>
+   <h2 class="c1">Header2</h2>
+
+    
+     <input type="button" value="Add para">
+  </body> 
+</html>
