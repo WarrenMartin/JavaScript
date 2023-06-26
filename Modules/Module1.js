b24d9cb76939c1c2d501ba66090a63a9da2d2905
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-06-26 08:58:25 +0530

    Create Module1.js

---
+++ Modules/Module1.js
@@ -0,0 +1,17 @@
+/* Module contains 4 things
+1. Var
+2. func
+3. Json
+4. classes
+*/
+
+exports.sname='Mayur'
+
+exports.greet =()=>{
+    console.log("Hello Node")
+}
+
+
+module.exports.morning=()=>{
+    console.log("Hello module.exports")
+}
