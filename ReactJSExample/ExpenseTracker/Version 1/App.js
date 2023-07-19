c058b42e161b889f9df0e65b87d5d71e1dfc85c1
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-07-19 23:34:53 +0530

    Create App.js

---
+++ ReactJSExample/ExpenseTracker/Version 1/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './Component/ExpenseItem';
+import Person from './Component/Person';
+
+function App() {
+
+  let expenses =[
+
+    {expDate:new Date(2022,7,14),expTitle:"School Fees",expAmount:45000},
+    {expDate:new Date(2020,4,14),expTitle:"MBA Fees",expAmount:150000},
+    {expDate:new Date(2022,7,14),expTitle:"Furniture",expAmount:55000}
+    
+
+  ]
+  
+  return (
+    <div>
+      <h1>Welcome to html</h1>
+      <ExpenseItem expDate={expenses[0].expDate} expTitle={expenses[0].expTitle} expAmount={expenses[0].expAmount}></ExpenseItem >
+      <ExpenseItem expDate={expenses[1].expDate} expTitle={expenses[1].expTitle} expAmount={expenses[1].expAmount}></ExpenseItem >
+      <ExpenseItem expDate={expenses[2].expDate} expTitle={expenses[2].expTitle} expAmount={expenses[2].expAmount}></ExpenseItem >
+
+      {/* <Person name ="Warr"/> */}
+      
+      
+    </div>
+  );
+
+}
+
+export default App;
